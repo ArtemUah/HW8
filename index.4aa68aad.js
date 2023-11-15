c1ff11de1f87ea7fdf5e249b39c2140afae5121b
ArtemUah <111749288+ArtemUah@users.noreply.github.com>
2023-11-15 17:28:01 +0000

    Deploying to gh-pages from @ ArtemUah/HW8@03a45453953a446045d5fd4e09e0939150b30c66 🚀

--- index.4aa68aad.js
+++
@@ -1,2 +0,0 @@
-console.log("hello"),document.querySelector(".title").textContent=" Goodbuy";
-//# sourceMappingURL=index.4aa68aad.js.map
--- index.6b25c106.js
+++
@@ -1,2 +0,0 @@
-console.log("hello");document.querySelector(".title").textContent=" Goodbuy";
-//# sourceMappingURL=index.6b25c106.js.map
